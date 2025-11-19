b2064f44b632ba9eacb03287b14072eea0d166b9
Mark <mark_g_king@mail.com>
2025-11-19 09:37:54 -0800

    add regular expressions

--- marksan.py
+++ marksan.py
@@ -4,6 +4,24 @@ with open("bookmarks.html", "r") as file:
 	content = file.read()
 	# print(content)
 
-	for line in content:
-		print(line)
-		print()
+	# for line in content:
+	# 	line = content.readline()
+	# 	print(line.strip())
+	# 	print()
+
+# with open("bookmarks.html", "r") as file:
+# 	for line in file:
+# 		line = file.readline()
+# 		print(line)
+# 		print()
+pat = "<A HREF=.+</A>"
+
+res = re.findall(pat,content)
+if res:
+	print("yes")
+else:
+	print("no")
+
+for mat in res:
+	a = re.search("<A HREF=(\"\S+\")", mat)
+	print(a)
